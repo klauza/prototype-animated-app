86910a37acd40add6745c23d7e4dc59dfb361165
klauza <klauza.uk@gmail.com>
2020-02-06 11:30:03 +0000

    add mouse scroll & mobile swipe to toggle views

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react';
 import { AbsoluteWrapper } from './reusable';
+import Swipe from 'react-easy-swipe';
 import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring'
 import ModalForm from './ModalForm';
@@ -67,7 +68,9 @@ const Home = () => {
   // Modal form
   const [modalDelay, setModalDelay] = React.useState(false);
   const [modal, setModal] = React.useState(false);
-
+  // scrolling
+  const [index, setIndex] = React.useState(0);
+  const [blockSwipe, setBlockSwipe] = React.useState(false);
 
   const props = useSpring({ 
     height: open ? "85vh" : "0"
@@ -96,44 +99,100 @@ const Home = () => {
     setModal(!modal);
   }
 
+  // PC SWIPE/mouse-SCROLL
+  const handleScroll = (e) => {
+    let direction;
+
+    // detect if firefox
+    if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1){
+      direction = -e.deltaY;
+    } else{
+      direction = e.nativeEvent.wheelDelta;
+    }
+   
+    if(!blockSwipe){
+      // next
+      if(direction < 0 && open){
+        toggleOpen();
+        blockFromSwipe(true); 
+      }
+     // prev
+      if(direction > 0 && !open){
+        toggleOpen();
+        blockFromSwipe(true); 
+      }
+    } else{
+      return;
+    }
+  }
+
+  // MOBILE SWIPE
+  const onSwipeMove = (position, event) => {
+    if(!blockSwipe){
+      // next
+      if(position.y < -75 && open){
+        toggleOpen();
+        blockFromSwipe(true); 
+      }
+     // prev
+      if(position.y > 75 && !open){
+        toggleOpen();
+        blockFromSwipe(true); 
+      }
+    } else{
+      return;
+    }
+  }
+
+  // BLOCK FROM SCROLL
+  const blockFromSwipe = () => {
+    // block
+    setBlockSwipe(true);
+
+    // remove block after 1s
+    setTimeout(()=>{
+      setBlockSwipe(false);
+    }, 1000)
+  }
 
   return (
     <AbsoluteWrapper>
       <ModalForm modalOpen={modal}/>
 
       
+      <Swipe onSwipeMove={onSwipeMove}>
+        <ExternalWrapper onWheel={(e)=>handleScroll(e)}>
+          <Wrapper>
 
-      <ExternalWrapper>
-        <Wrapper>
-
-          <animated.div className="body-top" style={props}>
-            <animated.div className="body-top__developer" style={propsOpacity}>
-                <p>KLAUZA</p>
-                <p>Portfolio</p>
-                <p>Web Developer</p>
+            <animated.div className="body-top" style={props}>
+              <animated.div className="body-top__developer" style={propsOpacity}>
+                  <p>KLAUZA</p>
+                  <p>Portfolio</p>
+                  <p>Web Developer</p>
+              </animated.div>
+              <animated.div className="body-top__SVG" style={propsOpacity}>SVG</animated.div>
             </animated.div>
-            <animated.div className="body-top__SVG" style={propsOpacity}>SVG</animated.div>
-          </animated.div>
 
-          <div className="body-bottom">
-            <span>15vh</span> <button onClick={toggleOpen}>Squash</button>
-          </div>
+            <div className="body-bottom">
+              <span>15vh</span> <button onClick={toggleOpen}>Squash</button>
+            </div>
 
-        </Wrapper>
+          </Wrapper>
 
-        <Wrapper>
-          <div style={{height: '85vh'}}>
+          <Wrapper>
+            <div style={{height: '85vh'}}>
 
-            <AboutMe>
-              <animated.div style={propsTwo} className="block-1">I am ready for new challenges</animated.div>
-              <animated.div style={propsTwo} className="block-2">My motivation and current goals</animated.div>
-              <animated.button style={propsTwo} onClick={openModalForm}>Ask me anything</animated.button>
-            </AboutMe>
+              <AboutMe>
+                <animated.div style={propsTwo} className="block-1">I am ready for new challenges</animated.div>
+                <animated.div style={propsTwo} className="block-2">My motivation and current goals</animated.div>
+                <animated.button style={propsTwo} onClick={openModalForm}>Ask me anything</animated.button>
+              </AboutMe>
 
-          </div>
-        </Wrapper>
+            </div>
+          </Wrapper>
 
-      </ExternalWrapper>
+        </ExternalWrapper>
+      </Swipe>
     </AbsoluteWrapper>
   )
 }
