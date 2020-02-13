864179569df955741e444a4def9d2845f391e154
klauza <klauza.uk@gmail.com>
2020-02-13 18:05:12 +0000

    init a boilerplate for animated page

--- src/components/PageOne.js
+++ src/components/PageOne.js
@@ -1,22 +1,69 @@
 import React from 'react'
 import { AbsoluteWrapper } from './reusable';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, useTrail, animated } from 'react-spring';
 import styled from 'styled-components';
 import Toggle from './Toggle';
 
+const Wrapper = styled.div`
+  min-height: 100vh;
+  background: lightsalmon;
+  margin-top: 50px;
+  h1{
+    text-align: center;
+    padding-top: 50px;
+  }
 
+  .trails-main {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  /* overflow: hidden; */
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+`;
 const Front = styled(animated.div)`
-  background: yellow;
+
 `;
 
 const PageOne = () => {
 
+
+  const [toggle, set] = React.useState(false);
+  React.useEffect(()=>{
+    setTimeout(()=>{
+      set(true);
+
+    }, 100)
+    return () => {
+      set(false);
+    }
+  }, [])
+
+const items = ['Lorem', 'ipsum', 'dolor', 'asd']
+  const config = { mass: 5, tension: 2000, friction: 200 }
+
+  const trail = useTrail(items.length, {
+    config,
+    opacity: toggle ? 1 : 0,
+    x: toggle ? 0 : 150,
+    height: toggle ? 80 : 0,
+    from: { opacity: 0, x: 150, height: 0 },
+  })
+
   const fade = useSpring({
     from: {
-      opacity: 0
+      trail: 1000,
+      opacity: 0,
+      transform: "translateY(100px)"
     },
     to: {
-      opacity: 1
+      trail: 1000,
+      opacity: 1,
+      transform: "translateY(0px)"
     }
   })
   // const fade = useSpring({ from: { opacity: 0 }, opacity: 1 });
@@ -25,12 +72,28 @@ const PageOne = () => {
 
   return (
     <AbsoluteWrapper>
-      <Front style={fade}>
-        1
-      </Front>
-      <main>
-        <Toggle />
-      </main>
+      <Wrapper>
+        <Front style={fade}>
+          <h1>Page one</h1>
+        </Front>
+
+        <div className="trails-main" onClick={() => set(state => !state)}>
+          <div>
+            {trail.map(({ x, height, ...rest }, index) => (
+              <animated.div
+                key={items[index]}
+                className="trails-text"
+                style={{ ...rest, transform: x.interpolate(x => `translate3d(${x}px, ${x}px, 0)`) }}>
+                <animated.div style={{ height }}>{items[index]}</animated.div>
+              </animated.div>
+            ))}
+          </div>
+        </div>
+
+        {/* <main>
+          <Toggle />
+        </main> */}
+      </Wrapper>
     </AbsoluteWrapper>
   )
 }
