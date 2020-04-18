caa77feb621c3a3f6c8a43d59aa6383064c9ee18
Michal Klauza <klauza.uk@gmail.com>
2020-04-18 12:03:42 +0100

    adjust some css on Information route

--- src/components/Information/InformationCSS.js
+++ src/components/Information/InformationCSS.js
@@ -2,4 +2,12 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   background: orange;
+  height: 100vh;
+
+  .info-header{
+    display: flex; flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2.5px;
+  }
 `;
--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { AbsoluteWrapper } from '../reusable';
+import { NavLink } from 'react-router-dom';
+import { AbsoluteWrapper, BackButton } from '../reusable';
 import { Container } from './InformationCSS';
 
 const Information = () => {
   return (
     <AbsoluteWrapper>
       <Container>
-        <p>Information</p>
+        <div className="info-header">
+          <p>Information</p>
+          <NavLink exact to={{pathname: '/', state: 0}}><BackButton text="button" icon=">" /></NavLink>
+        </div>
+        <div className="info-content">
+          <p>content_1</p>
+          <p>content_2</p>
+        </div>
       </Container>
     </AbsoluteWrapper>
   )
