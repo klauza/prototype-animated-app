7a1d567d1188f3f0ea3a7cc0a198e895db986fd1
Michal Klauza <klauza.uk@gmail.com>
2020-04-22 16:46:19 +0100

    adjust css for NotFound component route

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { AbsoluteWrapper } from '../reusable';
+import { Web } from '../../Icons';
+
+const Wrapper = styled.div`
+  background: #a5c2b3;
+  position: relative;
+  width: 100%; height: 100vh;
+  
+  .not-found__content{
+    position: absolute;
+    top: 50%; left: 50%;
+    transform: translate(-50%, -50%);
+    span{
+      color: white;
+      font-size: 2.5em;
+    }
+  }
+`;
 
 const NotFound = () => {
   return (
-    <div>
-      page is not found
-    </div>
+    <AbsoluteWrapper>
+      <Wrapper>
+        <div className="not-found__content">
+          <span>page is not found</span>
+          <Web />
+        </div>
+      </Wrapper>
+    </AbsoluteWrapper>
   )
 }
 
