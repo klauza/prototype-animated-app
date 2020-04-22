24dc50d4f74bbfd8f1b14467c424a7ab8f6fde05
Michal Klauza <klauza.uk@gmail.com>
2020-04-22 17:22:28 +0100

    update reusable BackButton, adjust redux animation

--- src/components/reusable/BackButton.js
+++ src/components/reusable/BackButton.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-// import history from '../../history';
+import history from '../../history';
+import { isMobile } from "react-device-detect";
+
+// redux
+import { connect } from 'react-redux';
+import { updt_animation_direction } from '../../actions/routesActions';
+// redux
+
 
 const Btn = styled.button`
   width: 100px;
@@ -8,15 +15,33 @@ const Btn = styled.button`
 
   display: block;
   border: 1px solid red;
+
+  position: absolute;
+  right: 0; 
+
+  top: ${props => props.isAndroid ? "50%" : "0%"};
+  transform: ${props => props.isAndroid ? "translate(0, -50%)" : "translate(0, 0)"};
 `;
 
-const BackButton = (props) => {
+// parent of this component must be relative
+const BackButton = ({display, text, icon, updt_animation_direction}) => {
+  // Back home button
+  const backFunc = () => {
+    const direction = display === "right" ? "to_left" : display === "left" && "to_right";
+    // console.log(direction);
+    updt_animation_direction(direction);
+
+    history.push({
+      pathname: '/',
+      state: 0
+    })
+  }
   return (
-    <Btn>
-      {props.text}
-      {props.icon && props.icon}
+    <Btn isAndroid={isMobile} onClick={backFunc} display={display}>
+      {text}
+      {icon && icon}
     </Btn>
   )
 }
 
-export default BackButton
+export default connect(null, { updt_animation_direction })(BackButton)
