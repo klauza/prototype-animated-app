05e6d368f79a2d2b8d07c0448f2e475c5dbd0902
Michal Klauza <klauza.uk@gmail.com>
2020-04-27 23:09:00 +0100

    contact tests

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -13,18 +13,22 @@ const Hero = ({ animationDirection }) => {
 
   return (
     <Wrapper>
-      <form name="contact" method="POST" data-netlify="true">
-        <input type="hidden" name="form-name" value="contact" />
-        <p>
-          <label>Email: <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message"></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+        <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
         <Spring
           config={generalAnimation}
           delay={250}
